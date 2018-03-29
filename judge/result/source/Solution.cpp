9ac97c947de0272a20a5745d35e468d867941738
sanjeev_26 <sanjeevkumar201418@gmail.com>
2018-03-29 10:46:17 +0530

    app-navbar removed

--- judge/result/source/Solution.cpp
+++ judge/result/source/Solution.cpp
@@ -1,33 +1,48 @@
-#include<stdio.h>
-int main()
-{
-	int t,m,count=0,imt,i;
-	long long temp;
-	scanf("%d",&t);
-	//if((t>0)&&(t<6))
-	{
-	while(t--)
-	{
-		scanf("%d",&m);
-		long long a[m];
-		for(i=0;i<m;i++)
-		{
-			scanf("%lld",&a[i]);
-			//if(i>0&&temp==1)
-		} 
-		count=0;
-		temp=a[0];
-		for(i=0;i<m-1;i++)
-		{
-			if((a[i]!=temp)||(a[i]!=a[i+1]))
-			count++;
-			temp=a[i];
-		}
-		if(a[i]!=temp)
-		count++;
-		printf("%d\n",count);
-		
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<algorithm>
+#include<cmath>
+#include<set>
+using namespace std;
+
+const int M=500005;
+const int N=50005;
+
+int n,k;
+struct node{
+	int sz;
+	int mn[11],mx[11];
+	friend bool operator<(node a,node b){
+		for (int i=1;i<=k;i++)
+			if (a.mx[i]>b.mn[i]) return 0;
+		return 1;
 	}
-}
+}w;
+set<node> st;
+set<node>::iterator l,r;
+
+int main(){
+	scanf("%d%d",&n,&k);
+	for (int i=1;i<=n;i++){
+		w.sz=1;
+		for (int j=1;j<=k;j++){
+			scanf("%d",&w.mn[j]);
+			w.mx[j]=w.mn[j];
+		}
+		l=st.lower_bound(w);
+		r=st.upper_bound(w);
+		while (l!=r){
+			for (int j=1;j<=k;j++){
+				w.mn[j]=min(w.mn[j],(*l).mn[j]);
+				w.mx[j]=max(w.mx[j],(*l).mx[j]);
+			}
+			w.sz+=(*l).sz;
+			st.erase(l++);
+		}
+		st.insert(w);
+		printf("%d\n",(*st.rbegin()).sz);
+	}
 	return 0;
-} 
+}
